67bcafdaad95097fcc80f8aeed8ddbc273df8a7f
Bach Ngoc Son <robin@cinnamon.is>
2018-07-06 14:28:04 +0700

    Define title format

--- inc/pdf_utils.hpp
+++ inc/pdf_utils.hpp
@@ -5,6 +5,44 @@
 #include <memory>
 #include <poppler/CharTypes.h>
 
+struct TitleFormat {
+        enum class CASE {ALL_UPPER, FIRST_ONLY_UPPER, ALL_LOWER};
+        enum class FONT_STYLE {NONE, BOLD, ITALIC, UNDERLINE, BOLD_ITALIC, BOLD_UNDERLINE, ITALIC_UNDERLINE, BOLD_ITALIC_UNDERLINE};
+        enum class PREFIX {NONE, BULLET, NUMBERING};
+        enum class EMPHASIZE_STYLE {NONE, SINGLE_QUOTE, DOUBLE_QUOTE};
+
+        double font_size;
+        std::string font_family;
+        FONT_STYLE font_style;
+        CASE title_case;
+        PREFIX prefix;
+        std::string prefix_format;
+        EMPHASIZE_STYLE emphasize_style;
+        bool same_line_with_content;
+
+        bool operator==(const TitleFormat& title_format) {
+            return font_size == title_format.font_size &&
+                   font_style == title_format.font_style &&
+                   title_case == title_format.title_case &&
+                   prefix == title_format.prefix &&
+                   emphasize_style == title_format.emphasize_style &&
+                   font_family.compare(title_format.font_family) == 0 &&
+                   prefix_format.compare(title_format.prefix_format) == 0 &&
+                   !(same_line_with_content ^ title_format.same_line_with_content);
+        }
+
+        bool operator!=(const TitleFormat& title_format) {
+            return font_size != title_format.font_size ||
+                   font_style != title_format.font_style ||
+                   title_case != title_format.title_case ||
+                   prefix != title_format.prefix ||
+                   emphasize_style != title_format.emphasize_style ||
+                   font_family.compare(title_format.font_family) != 0 ||
+                   prefix_format.compare(title_format.prefix_format) != 0 ||
+                   (same_line_with_content ^ title_format.same_line_with_content);
+        }
+};
+
 struct TextBlockInformation {
     bool is_page_number = false;
     bool has_title = false;
